ce7d1646badc45703aa10c1deccbd6e7c3aef991
Kytoii <kytoii.conect@gmail.com>
2025-11-29 17:49:37 +0000

    feat: melhora a comunicação dos módulos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// -------------------
+// Developed by Kytoii
+
+// Meu código é minha vida -
+// My code is my life -
+
+mod logic;  // Módulo para lógica
+mod io;     // Módulo de entrada
+mod output; // Módulo de saída
+mod utils;  // Utilitários
+
 fn main() {
-    println!("Hello, world!");
+
+    // Essa é a função principal.
+    // O sistema nervoso do nosso
+    // programa.
+
+    utils::clear();
+    output::init(); // Diz ao output que o 
+                    // programa foi iniciado.
+
+    // Pega a entrada do usuário.
+    let input: i32 = io::input();
+
 }
